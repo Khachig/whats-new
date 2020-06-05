1645374f4f07041c251fc9068975d872998aeb9e
Khachig Ainteblian <ainteblian99@gmail.com>
2020-06-05 00:31:33 -0400

    add basic interface

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
-from flask import Flask, jsonify, request
+from flask import jsonify
+from typing import Dict, Optional, Union
 
 from app import app
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({'data': 'Hello World'})
+
+def get_articles(data: Dict[str, Union[str, int]]) -> Dict[str, str]:
+    articles = [{'article': 'https://example.com', 'image': 'https://image.com'} for i in range(data['number'])]
+    return articles
+
+
+@app.route('/website=<website>/category=<category>')
+@app.route('/website=<website>/category=<category>/number=<int:number>')
+@app.route('/website=<website>/category=<category>/date_range=<date_range>')
+@app.route('/website=<website>/category=<category>/number=<int:number>/date_range=<date_range>')
+def get_query(website: str, category: str, number: int = 3, date_range: Optional[str] = None):
+
+    data = {'website': website,
+            'category': category,
+            'number': number,
+            'date_range': date_range}
+
+    articles = get_articles(data)
+
+    return jsonify(articles)
+
+
+@app.errorhandler(404)
+def not_found(arg):
+    return jsonify({'error': 'url not found'}), 404
